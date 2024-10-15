a5b8e86c1ef531726c31c20fc8205f9b6f80a2be
William F Basener <wb8by@virginia.edu>
2024-10-15 15:14:41 -0400

    added more images and pushed latest version to PyPi

--- hyperspectral_gta_data/data_download.py
+++ hyperspectral_gta_data/data_download.py
@@ -2,16 +2,14 @@ from zipfile import ZipFile
 import gdown
 import os
 import configparser
-
-
-
+from pathlib import Path
 
         
 
 class download:
     def __init__(self, data_name):
-        self.create_config(self)
-        self.read_settings(self)
+        self.dir_home = Path.home()
+        self.get_data_dir()
         
         # Dictionary to hold filenmae-id-data_type information
         self.image_dict = {
@@ -22,8 +20,6 @@ class download:
             'PaintDetectionUPWINS': ['Vegetation_Baccharis_halmifolia_UPWINS', '1WX_efoG5iIIYjg5Juh9tDz-Tk1BrElrk', 'image']
         }
         
-        https://drive.google.com/file/d//view?usp=drive_link
-        https://drive.google.com/file/d//view?usp=drive_link
         # determine the file information
         fname, fid, file_type = self.image_dict[data_name]
         
@@ -38,53 +34,42 @@ class download:
         self.download_unzip(fname, fid);
         
     
-    def read_settings(self):
+    def get_data_dir(self):    
         # Create a ConfigParser object
         config = configparser.ConfigParser()
-
         # Read the configuration file
-        config.read('config.ini')
-
-        # Access values from the configuration file
-        debug_mode = config.getboolean('General', 'debug')
-        log_level = config.get('General', 'log_level')
-        db_name = config.get('Database', 'db_name')
-        db_host = config.get('Database', 'db_host')
-        db_port = config.get('Database', 'db_port')
-
-        # Return a dictionary with the retrieved values
-        config_values = {
-            'debug_mode': debug_mode,
-            'log_level': log_level,
-            'db_name': db_name,
-            'db_host': db_host,
-            'db_port': db_port
-        }
-            
-        # Print the retrieved values
-        print(config_values['debug_mode'])
-        print(sonfig_values['log_level'])
-        print(config_values['db_name'])
-        print(config_values['db_host'])
-        print(config_values['db_port'])
+        if os.path.isfile(self.dir_home/'hsi_data_config.ini'):
+            config.read(self.dir_home/'hsi_data_config.ini')
+        else:
+            self.set_data_dir('.')
+            config.read(self.dir_home/'hsi_data_config.ini')
+        # Access the data dir value from the configuration file
+        self.data_dir = config.get('General', 'data_dir')
         
                                     
-    def create_config(self):
-        config = configparser.ConfigParser()
-        # Add sections and key-value pairs
-        config['General'] = {'debug': True, 'log_level': 'info'}
-        config['Database'] = {'db_name': 'example_db',
-                            'db_host': 'localhost', 'db_port': '5432'}
-        # Write the configuration to a file
-        with open('config.ini', 'w') as configfile:
-            config.write(configfile)           
+    def set_data_dir(self, data_dir):
+        try:
+            config = configparser.ConfigParser()
+            # Add sections and key-value pairs
+            config['General'] = {'data_dir': data_dir}
+            # Write the configuration to a file
+            with open(self.dir_home/'hsi_data_config.ini', 'w') as configfile:
+                config.write(configfile)   
+        except: 
+            print('Directory value not valid.')        
+            config = configparser.ConfigParser()
+            # Add sections and key-value pairs
+            config['General'] = {'data_dir': '.'}
+            # Write the configuration to a file
+            with open(self.dir_home/'hsi_data_config.ini', 'w') as configfile:
+                config.write(configfile)   
             
             
     def download_unzip(self, fname, fid):
             
         if not os.path.isdir('spectral_images/'+fname):
             # Download the zip files of the image.
-            fnameZip = 'spectral_images/'+fname+'.zip'
+            fnameZip = self.data_dir+'/'+'spectral_images/'+fname+'.zip'
             if not os.path.isfile(fnameZip):
                 gdown.download(id=fid, output=fnameZip)
             else:
@@ -94,13 +79,10 @@ class download:
             with ZipFile(fnameZip, 'r') as zObject: 
                 zipped_filenames = zObject.namelist()
                 zObject.extractall( 
-                    path='spectral_images/'+fname) 
+                    path=self.data_dir+'/'+'spectral_images/'+fname) 
             for zfname in zipped_filenames:
-                print(f'File saved as: spectral_images/{fname}/{zfname}')
+                print(f'File saved as: {self.data_dir}/spectral_images/{fname}/{zfname}')
             
             # Delete the zip file
             os.remove(fnameZip)
-    
-    
-    
     
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'hyperspectral_gta_data',         # How you named your package folder (MyLib)
   packages = ['hyperspectral_gta_data'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '1.0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Module for downloading hyperspectral imagery and libraries',   # Give a short description about your library
   author = 'Bill Basener',                   # Type in your name
@@ -29,3 +29,7 @@ setup(
     'Programming Language :: Python :: 3.11',
   ],
 )
+
+# To build:
+#  > python -m build
+#  > python -m twine upload dist/*
